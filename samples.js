2a92e1bbe2e4953472ecfc043b88f455a3150784
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-06-19 19:45:43 +0200

    initial push

---
+++ samples.js
@@ -0,0 +1,12 @@
+var samples = [
+    '\\nabla\\f(\\x) = 2\\x',
+    '\\v = \\partial\\x/\\partial\\t',
+    'lim_{x \\rightarrow \\infty} 1/x = 0',
+    'cos \\alpha = \\langle\\u,\\v\\rangle/\\Vert\\u\\Vert\\cdot\\Vert\\v\\Vert',
+    '\\Delta_t \\leq (1 - \\kappa)^t\\Delta_0',
+    'exp(\\pi\\i) = -1',
+    '\\Phi = \\int \\mathbf{E} \\cdot d\\mathbf{A}',
+    '\\partial^2\\u/\\partial\\x^2 + \\partial^2\\u/\\partial\\y^2 = 0',
+    '\\mathcal{N}(\\x; \\mu, \\sigma^2) = exp(-(\\x - \\mu)^2/\\sigma^2)/\\sqrt\\overline2\\overline\\pi\\sigma',
+    '\\mathbb{E}[(\\x-\\mu)^2] = \\sigma^2',
+]
